d18cc79725477e83088e578de506b22655db7e40
Shehryar <shehryarhussainkhatri@gmail.com>
2024-09-29 16:40:01 +0500

    Added ChatBot
    -Chatbot added
    -env for openai is required
    -Link used for navigating the pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 "use client";
 import Login from "./(pages)/Login/page";
-
+import Link from "next/link";
 
 export default function Home() {
-
-
   return (
     <>    
         <Login />
-
+        <Link href="/chatbot">Go to the chatbot page from here</Link>
     </>
   );
 }
