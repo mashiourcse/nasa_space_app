59cf4198f2e5992d6a690eeba1f6b24a56135b8a
mashiourcse <mashiour.cseiiuc@gmail.com>
2024-09-30 23:56:03 +0600

    nextconfig updated for openai

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+    env: {
+        OpenAI: process.env.OPEN_AI,
+      },
 };
 
 export default nextConfig;
--- src/redux/Api/baseApi.js
+++ src/redux/Api/baseApi.js
@@ -4,7 +4,7 @@ import { tags } from "../Tag/Tag";
 const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://cors-anywhere.herokuapp.com/https://exoplanetarchive.ipac.caltech.edu/TAP/",  
+    baseUrl: "https://cors-anywhere.herokuapp.com/https://exoplanetarchive.ipac.caltech.edu/TAP/",  // temporary
    // baseUrl: process.env.NASA_TAP_URL,
     prepareHeaders: (headers) => {
      //https://cors-anywhere.herokuapp.com/
