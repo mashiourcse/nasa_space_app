0389d495b6f677e65147a10e39025d8e88b2e0e9
Mashiour Rahman <42815237+mashiourcse@users.noreply.github.com>
2024-10-07 11:33:08 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -102,7 +102,7 @@ export default function Home() {
                       ? " (" +
                         Math.round(getFarthestPlanet[0].sy_dist * 3.26156) +
                         " light years away)"
-                      : " (Unknown)")}</h2>
+                      : " (Distance Unknown)")}</h2>
               </Button>
               
             </Card>
